a8f47ca80d480fb9437c96b667fec6a621285cd2
Lee Duong <vietlee.duong@gmail.com>
2017-01-14 22:50:54 +0000

    Added tests for AddressBookParser and code to read from file

--- src/main/java/AddressBookParser.java
+++ src/main/java/AddressBookParser.java
@@ -1,4 +1,56 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-public class AddressBookParser {
+/**
+ * <p>
+ * The purpose of this class is to process the address book file and return a list of contacts.
+ * </p>
+ * 
+ * @author Lee Duong
+ */
+public class AddressBookParser
+{
 
+    private String fileName;
+
+    /**
+     * The Address book constructor.
+     * 
+     * @param fileName
+     *            The file name to process.
+     */
+    public AddressBookParser(String fileName)
+    {
+        this.fileName = fileName;
+    }
+
+    /**
+     * Retrieves a list of contacts from the address book file.
+     *
+     * @return a list of contacts.
+     */
+    public List<Contact> getContacts()
+    {
+        List<Contact> contacts = new ArrayList<Contact>();
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(fileName).getFile());
+        try
+        {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine())
+            {
+                String line = scanner.nextLine();
+                String[] contactProperties = line.split(", ");
+                contacts.add(new Contact());
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        return contacts;
+    }
 }
--- src/test/java/AddressBookParserTest.java
+++ src/test/java/AddressBookParserTest.java
@@ -1,4 +1,35 @@
+import static org.junit.Assert.assertEquals;
 
-public class AddressBookParserTest {
+import org.junit.Test;
 
+/**
+ * <p>
+ * The purpose of this class is to test {@link AddressBookParser}.
+ * </p>
+ * 
+ * @author Lee Duong
+ */
+public class AddressBookParserTest
+{
+
+    @Test
+    public void getContactsWithEmptyContactFileShouldReturnEmptyList()
+    {
+        AddressBookParser addressBookParser = new AddressBookParser("AddressBookWithZeroContacts");
+        assertEquals(0, addressBookParser.getContacts().size());
+    }
+
+    @Test
+    public void getContactsWithOneContactFileShouldReturnOneContact()
+    {
+        AddressBookParser addressBookParser = new AddressBookParser("AddressBookWithOneContact");
+        assertEquals(1, addressBookParser.getContacts().size());
+    }
+
+    @Test
+    public void getContactsWithMultipleContactsFileShouldReturnMultipleContacts()
+    {
+        AddressBookParser addressBookParser = new AddressBookParser("AddressBookWithThreeContacts");
+        assertEquals(3, addressBookParser.getContacts().size());
+    }
 }
