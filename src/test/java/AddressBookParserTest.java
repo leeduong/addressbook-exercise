00b03f7981c33e6c9089ff7d6bcc35b8059a4348
Lee Duong <vietlee.duong@gmail.com>
2017-01-15 12:04:38 +0000

    Improved tests in AddressBookParserTest to look at Contact fields

--- src/test/java/AddressBookParserTest.java
+++ src/test/java/AddressBookParserTest.java
@@ -1,5 +1,9 @@
 import static org.junit.Assert.assertEquals;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.junit.Test;
 
 /**
@@ -13,6 +17,16 @@ public class AddressBookParserTest
 {
 
     @Test
+    public void constructorWithNullFileNameShouldThrowException()
+    {
+    }
+    
+    @Test
+    public void constructorWithInvalidFileNameShouldThrowException()
+    {
+    }
+    
+    @Test
     public void getContactsWithEmptyContactFileShouldReturnEmptyList()
     {
         AddressBookParser addressBookParser = new AddressBookParser("AddressBookWithZeroContacts");
@@ -20,16 +34,48 @@ public class AddressBookParserTest
     }
 
     @Test
-    public void getContactsWithOneContactFileShouldReturnOneContact()
+    public void getContactsWithOneContactFileShouldReturnOneContact() throws ParseException
     {
         AddressBookParser addressBookParser = new AddressBookParser("AddressBookWithOneContact");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yy");
+        Date expectedDate = simpleDateFormat.parse("16/03/77");
+        String expectedName = "Bill McKnight";
+        Gender expectedGender = Gender.MALE;
         assertEquals(1, addressBookParser.getContacts().size());
+        Contact contact = addressBookParser.getContacts().get(0);
+        assertEquals(expectedName, contact.getName());
+        assertEquals(expectedGender, contact.getGender());
+        assertEquals(expectedDate, contact.getDateOfBirth());
     }
 
     @Test
-    public void getContactsWithMultipleContactsFileShouldReturnMultipleContacts()
+    public void getContactsWithMultipleContactsFileShouldReturnMultipleContacts() throws ParseException
     {
         AddressBookParser addressBookParser = new AddressBookParser("AddressBookWithThreeContacts");
         assertEquals(3, addressBookParser.getContacts().size());
+        
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yy");
+        Date expectedDateOne = simpleDateFormat.parse("16/03/77");
+        String expectedNameOne = "Bill McKnight";
+        Gender expectedGenderOne = Gender.MALE;
+        Date expectedDateTwo = simpleDateFormat.parse("15/01/85");
+        String expectedNameTwo = "Paul Robinson";
+        Gender expectedGenderTwo = Gender.MALE;
+        Date expectedDateThree = simpleDateFormat.parse("20/11/91");
+        String expectedNameThree = "Gemma Lane";
+        Gender expectedGenderThree = Gender.FEMALE;
+        
+        Contact contactOne = addressBookParser.getContacts().get(0);
+        assertEquals(expectedNameOne, contactOne.getName());
+        assertEquals(expectedGenderOne, contactOne.getGender());
+        assertEquals(expectedDateOne, contactOne.getDateOfBirth());        
+        Contact contactTwo = addressBookParser.getContacts().get(1);
+        assertEquals(expectedNameTwo, contactTwo.getName());
+        assertEquals(expectedGenderTwo, contactTwo.getGender());
+        assertEquals(expectedDateTwo, contactTwo.getDateOfBirth());
+        Contact contactThree = addressBookParser.getContacts().get(2);
+        assertEquals(expectedNameThree, contactThree.getName());
+        assertEquals(expectedGenderThree, contactThree.getGender());
+        assertEquals(expectedDateThree, contactThree.getDateOfBirth());
     }
 }
